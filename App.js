58cf6a92f6e3baf403e10c765dd80f844900faa8
lucas <laguero90@gmail.com>
2021-03-29 11:58:32 -0500

    levanto los datos y los guardo en el hook, consologueo a ver si estan ok

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useEffect, useState} from 'react';
+import { StyleSheet, Text, View , Button} from 'react-native';
+import axios from 'axios'
 
 export default function App() {
+
+    //importing data
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+      
+      axios
+      .get(`https://0q27loouph.execute-api.us-east-1.amazonaws.com/`)
+      .then((res) => setData(res.data))
+      .catch((error) => alert("Error to load data"))
+
+      return () => {
+          setData()
+      }
+  }, [])
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title='consoleLog' onPress={()=>console.log(data)}/>
       <StatusBar style="auto" />
     </View>
   );
